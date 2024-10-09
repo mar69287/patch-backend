33af2e47c59a2017c620f39e46b4b67659b18669
mar69287 <marcoa.ruiz97@gmail.com>
2024-10-08 20:14:57 -0700

    create game model

--- models/game.js
+++ models/game.js
@@ -1,22 +1,26 @@
 import mongoose from 'mongoose';
 
-const Game = new mongoose.Schema({
-    name: {type: String, required: true},
-    email: {
-      type: String,
-      unique: true,
-      trim: true,
-      lowercase: true,
-      required: true
-    },
-    password: {
-      type: String,
-      required: true
-    }
-}, {
-    timestamps: true,
+// Define the schema for the patch notes
+const PatchNoteSchema = new mongoose.Schema({
+    title: { type: String }, 
+    content: { type: String }, 
+    releaseDate: { type: String } 
 });
 
-const GameSchema = mongoose.model('Game', Game)
+// Define the schema for the game
+const GameSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    slug: { type: String, unique: true, required: true },
+    description: { type: String }, 
+    background_image: { type: String },
+    platforms: { type: [String], required: true }, 
+    genres: { type: [String], required: true },
+    patchNotes: [PatchNoteSchema], 
+}, {
+    timestamps: true, 
+});
 
-export default GameSchema;
+const Game = mongoose.model('Game', GameSchema);
+
+export default Game;
+
