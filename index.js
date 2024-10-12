4b954fc83579b3ff15aaa5b78cb78c71255d5d42
Marco Ruiz Jr <marcoa.ruiz97@gmail.com>
2024-10-12 14:21:05 -0700

    add bots and command.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import morgan from 'morgan';
-import { bot, initializeBot } from './discord/discordClient.js';
+import { initializeBot, initializeTrackerBot } from './discord/discordClient.js';
+import { registerCommands } from './discord/commands/registerCommands.js';
 
 import connectDB from './config/database.js';
 import gameRoutes from './routes/gameRoutes.js';
@@ -15,24 +16,28 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.use('/api/v1/games', gameRoutes)
-// app.use('/api/v1/users', userRoutes)
+app.use('/api/v1/games', gameRoutes);
+// app.use('/api/v1/users', userRoutes);
 
 app.get('/', async (req, res) => {
-    res.send('hello from Server Side!')
+    res.send('hello from Server Side!');
 });
 
 const port = process.env.PORT || 8080;
 
 const startServer = async () => {
-    try {
-      connectDB(process.env.DATABASE_URL);
-      app.listen(port, () => console.log(`Express app running on port ${port}`));
-      initializeBot();
-      bot.login(process.env.DISCORD_BOT_TOKEN);
-    } catch (error) {
-      console.log(error);
-    }
+  try {
+    connectDB(process.env.DATABASE_URL);
+    app.listen(port, () => console.log(`Express app running on port ${port}`));
+
+
+    // await registerCommands();
+    initializeBot(); 
+    initializeTrackerBot(); 
+
+  } catch (error) {
+    console.log(error);
+  }
 };
-  
+
 startServer();
